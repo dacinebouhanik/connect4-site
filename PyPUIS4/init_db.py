7d9e2c67ce3e564a089a017da35039efe4225c2b
dacinebouhanik <127552981+dacinebouhanik@users.noreply.github.com>
2026-03-07 22:46:25 +0100

    init db auto

--- PyPUIS4/init_db.py
+++ PyPUIS4/init_db.py
@@ -1,35 +1,41 @@
 import psycopg2
 import os
 
-conn = psycopg2.connect(os.environ["DATABASE_URL"])
-cur = conn.cursor()
+database_url = os.environ.get("DATABASE_URL")
 
-cur.execute("""
-CREATE TABLE IF NOT EXISTS games (
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    lignes INT,
-    colonnes INT,
-    couleur_depart INT,
-    joueur_courant INT,
-    statut TEXT,
-    resultat TEXT,
-    confiance INT
-);
-""")
+# si on est en local et que la variable n'existe pas
+if database_url is None:
+    print("init_db ignoré : DATABASE_URL non définie (local)")
+else:
+    conn = psycopg2.connect(database_url)
+    cur = conn.cursor()
 
-cur.execute("""
-CREATE TABLE IF NOT EXISTS games_coups (
-    id SERIAL PRIMARY KEY,
-    game_id INT REFERENCES games(id),
-    coups TEXT,
-    coups_symetrique TEXT,
-    coups_canonique TEXT
-);
-""")
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS games (
+        id SERIAL PRIMARY KEY,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        lignes INT,
+        colonnes INT,
+        couleur_depart INT,
+        joueur_courant INT,
+        statut TEXT,
+        resultat TEXT,
+        confiance INT
+    );
+    """)
 
-conn.commit()
-cur.close()
-conn.close()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS games_coups (
+        id SERIAL PRIMARY KEY,
+        game_id INT REFERENCES games(id),
+        coups TEXT,
+        coups_symetrique TEXT,
+        coups_canonique TEXT
+    );
+    """)
 
-print("Tables créées")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print("Tables PostgreSQL vérifiées/créées")
